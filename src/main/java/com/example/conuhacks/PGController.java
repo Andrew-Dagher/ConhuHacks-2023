bcd4a6998ea9f484e1cfdd729ae82f1bb18f5090
StoneDagher <115055489+Andrew-Dagher@users.noreply.github.com>
2023-01-21 17:17:00 -0500

    PG creation

---
+++ src/main/java/com/example/conuhacks/PGController.java
@@ -0,0 +1,9 @@
+package com.example.conuhacks;
+
+import javafx.event.ActionEvent;
+
+public class PGController {
+    public void buttonGeneratePassword(ActionEvent actionEvent) {
+        System.out.println("poop");
+    }
+}
