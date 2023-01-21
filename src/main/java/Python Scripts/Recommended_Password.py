13352f0f3a7e01737a4607c823389f03aa4883ae
richardbadir <116977955+richardbadir@users.noreply.github.com>
2023-01-21 17:16:53 -0500

    Add files via upload

--- src/main/java/Python Scripts/Recommended_Password.py
+++ src/main/java/Python Scripts/Recommended_Password.py
@@ -1,37 +1,42 @@
 import random
 
 def Recommended_Password(words): #does the recommended password
+    Word_Counter=1
     password=[]# store the password as a list
     for letter in words: 
         password.append(letter)
     vowels="aeiouy"
-    while True:#make a random consonnant a Capital
-        i=random.randint(0,(len(password)-1))
-        if password[i] not in vowels and password[i] != " ": 
-            password[i]=password[i].upper()
-            break
+    
     
     spaces_replacements="._-,"
     vowel_replacements="0123456789!#$%&'()*+/:;<=>?@[\]^`{|}~"
-    for i in range(len(password)): #if there are spaces, replace them with a random character from the space_replacements character
+    for i in range(len(password)): # replace spaces with a random character from the space_replacements character
         if " "==password[i]:
+            Word_Counter+=1
             password[i]= spaces_replacements[random.randint(0,(len(spaces_replacements)-1))]
             
 
-     #change the variables to random characters
-    for i in range(len(vowels)):
-        for y in range(len(password)):
-            if vowels[i]==password[y]:
-                password[y]= vowel_replacements[random.randint(0,(len(vowel_replacements)-1))]
+     #change the variables to random characters, unless 4 words are used
+    if Word_Counter<4:
+        for i in range(len(vowels)):
+            for y in range(len(password)):
+                if vowels[i]==password[y]:
+                    password[y]= vowel_replacements[random.randint(0,(len(vowel_replacements)-1))]
 
     
+    if Word_Counter<4:
+        while True:#make a random consonnant a Capital
+            i=random.randint(0,(len(password)-1))
+            if password[i] not in vowels and password[i] != " ": 
+                password[i]=password[i].upper()
+                break
         
 
     spassword= ' '.join(password) #make he li a in
 
 
     print(spassword)
-        
+Recommended_Password("apple dictionnary sauce blabla")       
 
 
 
