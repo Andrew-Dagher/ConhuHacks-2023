daa55954cedf93f3ddaae912d3986a21ea034c5f
StoneDagher <115055489+Andrew-Dagher@users.noreply.github.com>
2023-01-21 12:58:07 -0500

    commited

--- src/main/java/Func/Utils.java
+++ src/main/java/Func/Utils.java
@@ -2,5 +2,11 @@ package Func;
 
 public class Utils {
 
-    static int  a = 0 ;
+   public static final String MAIN_COLOUR = "";
+   public static final String BAD_COLOUR = "";
+   public static final String GOOD_COLOUR = "";
+   public static final String BORDER_COLOUR = "";
+   public static final String SECONDARY1_COLOUR = "";
+   public static final String SECONDARY2_COLOUR = "";
+   public static final String CHECKBOX_COLOUR = "";
 }
