44db16a150d29f13a9561c2251d28b10ed70ea2e
AC <39713625+R0MULLUS@users.noreply.github.com>
2023-01-21 17:48:24 -0500

    a

--- src/main/java/com/example/conuhacks/PGController.java
+++ src/main/java/com/example/conuhacks/PGController.java
@@ -3,6 +3,7 @@ package com.example.conuhacks;
 import javafx.event.ActionEvent;
 
 public class PGController {
+    // c
     public void buttonGeneratePassword(ActionEvent actionEvent) {
         System.out.println("poop");
     }
